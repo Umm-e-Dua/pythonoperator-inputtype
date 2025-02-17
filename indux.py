df95aae909bca94ba12fbf72123aa8d9024d8314
Umm-e-Dua <160483094+Umm-e-Dua@users.noreply.github.com>
2025-02-18 02:21:44 +0500

    Add files via upload
    operator python coverd and logic and or operator and input type str int

---
+++ indux.py
@@ -0,0 +1,65 @@
+# print hellow word
+print("Hello world")
+
+# data type and variable type check str , num, bool, float , none
+
+name = "Ummedua"
+
+age = 18
+
+my_work = True
+
+final = 3.444
+
+super = None
+
+print(type(name))
+print(type(age))
+print(type(my_work))
+print(type(final))
+print(type(super))
+
+
+# Operators Arithmetic 
+print(1+6)
+print(1-6)
+print(2*2)
+print(5**3)
+print(1%4)
+
+# relational operator comparison  less then greater then not equal 
+
+print(2==2)
+print(5>2)
+print(5<2)
+print(4!=5)
+
+
+# assignment opeartor 
+a = 10
+a += 10 
+print(a)
+
+a = 10
+a -= 4
+print(a)
+
+a = 10
+a %= 5
+print(a)
+
+# logical operator 
+a = 20
+b = 10
+print (a >  b and b < a)
+print (a < b or b < a)
+print ( not a >  b )
+
+# type input python 
+a = int(input("enter your name:"))
+
+a = int(input("enter your name:"))
+print(type (a))
+
+
+
